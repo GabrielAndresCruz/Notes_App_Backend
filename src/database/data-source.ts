b062b85a02ae793632563518c81c9dc01ba064a1
GabrielAndresCruz <andrescruz048@hotmail.com>
2024-01-09 09:04:19 -0300

    App connected to DB

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost", // This IP direction is for locally running db and server. If you have a deployed server, you must be change the host server direction.
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  logging: ["query"],
+  synchronize: false, //synchronize - Indicates if database schema should be auto created on every application launch. Be careful with this option and don't use this in production - otherwise you can lose production data. This option is useful during debug and development. As an alternative to it, you can use CLI and run schema:sync command.
+  entities: ["src/database/entities/*.ts"],
+  subscribers: [],
+  migrations: ["src/database/migrations/*.ts"],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import * as dotenv from "dotenv";
 import app from "./app";
+import { AppDataSource } from "./database/data-source";
 
-app.listen(3000, () => {
-  console.log("Server is running in port 3000");
+dotenv.config();
+
+const PORT = process.env.APP_PORT;
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Data Source has been initialized");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization");
+    console.error(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
