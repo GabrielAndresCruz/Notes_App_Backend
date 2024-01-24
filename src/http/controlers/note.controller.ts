52955883ab6dc141e12359a4ed2c5b1ae5a5cb20
GabrielAndresCruz <andrescruz048@hotmail.com>
2024-01-24 12:19:36 -0300

    Create getAllNotes controller and route

---
+++ src/http/controlers/note.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../../database/data-source";
+import { Note } from "../../database/entities/note.entity";
+import { sendResponse } from "../../utils/responseHandlers";
+
+export class NoteController {
+  /* When you use private variables to encapsulate the logic, you must take two different ways:
+    1- Use bind inside the controller Class:
+        constructor () {
+            this.getFunction = this.getFunction.bind(this)
+        }
+       Or, use bind outside the controller Class, in note.route.ts file:
+        router.get("/note", noteController.getFunction.bind(getFunction))
+    2- Note arrow function to getFunctions. Arrow function can't use like constructors, and don't have .prototype property.
+  */
+
+  // Private class constant to avoid repetition every time it's needed in different functions.
+  private noteRepository = AppDataSource.getRepository(Note);
+
+  // Binding the controllers functions
+  constructor() {
+    this.getAllNotes = this.getAllNotes.bind(this);
+  }
+
+  // Controllers functions
+  async getAllNotes(req: Request, res: Response) {
+    const notes: Array<Note> = await this.noteRepository.find();
+    sendResponse(res, 200, notes, "Notes list");
+  }
+}
---
+++ src/routes/note.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { NoteController } from "../http/controlers/note.controller";
+import errorHandler from "../http/middleware/errorHandler.middleware";
+
+const router = express.Router();
+
+const noteController = new NoteController();
+
+// The errorHandler middleware catches errors without cluttering the controller logic with try-catch blocks.
+router.get("/", errorHandler(noteController.getAllNotes));
